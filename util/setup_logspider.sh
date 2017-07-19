a4841d5ca5f0550a6aadf125bdf64b31c747e889
Denis Lavrov <bahus.vel@gmail.com>
2017-07-19 13:13:14 +1200

    Added log to ignore

--- util/setup_logspider.sh
+++ util/setup_logspider.sh
@@ -3,6 +3,6 @@
 
 #add user
 ssh-copy-id -i ../id_rsa logspider@$1
-# enable multiplexing in ssh echo MaxSessions 8192 > /etc/ssh/sshd_config; service ssh restart
+# echo MaxSessions 8192 > /etc/ssh/sshd_config; service ssh restart
 # chmod g+s <directory>  //set gid
 #setfacl -d -m o::r /<directory>
