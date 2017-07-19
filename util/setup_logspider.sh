f979a2377a51a5eb246b49400aca400c73ac00a4
Denis Lavrov <bahus.vel@gmail.com>
2017-07-19 13:36:35 +1200

    Added some more ssh automation

--- util/setup_logspider.sh
+++ util/setup_logspider.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 
-#add user
-ssh-copy-id -i ../id_rsa logspider@$1
-# echo MaxSessions 8192 > /etc/ssh/sshd_config; service ssh restart
-# chmod g+s <directory>  //set gid
-#setfacl -d -m o::r /<directory>
+ssh root@$1 adduser --disabled-password --gecos "" logspider
+#ssh-copy-id -i ../id_rsa logspider@$1
+cat ../id_rsa.pub | ssh root@$1 "sudo -H -u logspider bash -c 'mkdir -p ~/.ssh; cat >>  ~/.ssh/authorized_keys'"
+
+ssh root@$1 "echo MaxSessions 8192 >> /etc/ssh/sshd_config; service ssh restart; chmod g+s /var/log; setfacl -d -m o::r /var/log"
